87818343c3f0fd6b29b280e4a5091cebf0a2c7f3
Dmitri Shuralyov <shurcooL@gmail.com>
2015-06-04 23:04:22 -0700

    Use more idiomatic/correct example output format.

--- main_test.go
+++ main_test.go
@@ -13,7 +13,7 @@ func ExampleMarkdown() {
 	os.Stdout.Write(github_flavored_markdown.Markdown(text))
 
 	// Output:
-	//<p>Hello world github/linguist#1 <strong>cool</strong>, and #1!</p>
+	// <p>Hello world github/linguist#1 <strong>cool</strong>, and #1!</p>
 }
 
 // An example of how to generate a complete HTML page, including CSS styles.
@@ -26,10 +26,10 @@ func ExampleMarkdown_completeHtmlPage() {
 	io.WriteString(w, `</article></body></html>`)
 
 	// Output:
-	//<html><head><meta charset="utf-8"><link href=".../github-flavored-markdown.css" media="all" rel="stylesheet" type="text/css" /><link href="//cdnjs.cloudflare.com/ajax/libs/octicons/2.1.2/octicons.css" media="all" rel="stylesheet" type="text/css" /></head><body><article class="markdown-body entry-content" style="padding: 30px;"><h1><a name="github-flavored-markdown" class="anchor" href="#github-flavored-markdown" rel="nofollow" aria-hidden="true"><span class="octicon octicon-link"></span></a>GitHub Flavored Markdown</h1>
+	// <html><head><meta charset="utf-8"><link href=".../github-flavored-markdown.css" media="all" rel="stylesheet" type="text/css" /><link href="//cdnjs.cloudflare.com/ajax/libs/octicons/2.1.2/octicons.css" media="all" rel="stylesheet" type="text/css" /></head><body><article class="markdown-body entry-content" style="padding: 30px;"><h1><a name="github-flavored-markdown" class="anchor" href="#github-flavored-markdown" rel="nofollow" aria-hidden="true"><span class="octicon octicon-link"></span></a>GitHub Flavored Markdown</h1>
 	//
-	//<p>Hello.</p>
-	//</article></body></html>
+	// <p>Hello.</p>
+	// </article></body></html>
 }
 
 func ExampleHeader() {
@@ -38,5 +38,5 @@ func ExampleHeader() {
 	os.Stdout.Write(github_flavored_markdown.Markdown(text))
 
 	// Output:
-	//<h2><a name="git-diff" class="anchor" href="#git-diff" rel="nofollow" aria-hidden="true"><span class="octicon octicon-link"></span></a>git diff</h2>
+	// <h2><a name="git-diff" class="anchor" href="#git-diff" rel="nofollow" aria-hidden="true"><span class="octicon octicon-link"></span></a>git diff</h2>
 }
